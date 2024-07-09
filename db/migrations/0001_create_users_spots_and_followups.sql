476772307f423d7e994ce2f4ae8cbe4f194f0950
bootleq <bootleq@gmail.com>
2024-07-09 15:32:08 +0800

    Start setup D1 and R2, some cleanup
    Allow using migration to create some tables.

---
+++ db/migrations/0001_create_users_spots_and_followups.sql
@@ -0,0 +1,34 @@
+-- Migration number: 0001 	 2024-07-06T07:42:53.263Z
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  email TEXT,
+  state TEXT,
+  desc TEXT,
+  created_at TEXT,
+  created_by INTEGER
+);
+
+DROP TABLE IF EXISTS spots;
+CREATE TABLE IF NOT EXISTS spots (
+  id INTEGER PRIMARY KEY,
+  state TEXT,
+  desc TEXT,
+  created_at TEXT,
+  created_by INTEGER,
+  lat TEXT,
+  lon TEXT
+);
+
+DROP TABLE IF EXISTS followups;
+CREATE TABLE IF NOT EXISTS followups (
+  id INTEGER PRIMARY KEY,
+  type TEXT,
+  user_id INTEGER,
+  spot_id INTEGER,
+  desc TEXT,
+  created_at TEXT,
+  created_by INTEGER
+);
