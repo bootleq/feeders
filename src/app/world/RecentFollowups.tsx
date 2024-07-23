0d978edc4e965021c65d197e6a8da6969efeec6f
bootleq <bootleq@gmail.com>
2024-07-23 22:49:08 +0800

    Minor view improvement, introduce jotai

--- src/app/world/RecentFollowups.tsx
+++ src/app/world/RecentFollowups.tsx
@@ -1,16 +1,85 @@
 "use client"
 
 import * as R from 'ramda';
-import { subDays, formatISO } from '@/lib/date-fp';
+import { useState, useEffect } from 'react';
+import { atom, useAtom, useSetAtom, useAtomValue } from 'jotai';
+import { subDays, formatISO, formatDistanceToNow } from '@/lib/date-fp';
 import { recentFollowups } from '@/models/spots';
 import { MapPinIcon } from '@heroicons/react/24/solid';
 
 type RecentFollowupsItemProps = Awaited<ReturnType<typeof recentFollowups>>[number];
 
-function SpotInfo({ spot }: { spot: RecentFollowupsItemProps | null}) {
+const viewItemAtom = atom<RecentFollowupsItemProps | null>(null);
+const setDefaultViewItemAtom = atom(
+  null,
+  (get, set, update: RecentFollowupsItemProps) => set(viewItemAtom, (value) => (R.isNil(value) ? update : value))
+)
+
+function StateLabel({ spotState, children }: {
+  spotState: string,
+  children?: React.ReactNode
+}) {
+  let cls = '';
+
+  switch (spotState) {
+    case 'dirty':
+      cls = 'bg-amber-950';
+      break;
+    case 'clean':
+      cls = 'bg-slate-400 opacity-70';
+      break;
+    case 'tolerated':
+      cls = 'bg-blue-700';
+      break;
+    default:
+      cls = 'bg-current';
+  }
+
   return (
-    <div className='bg-lime-300 aspect-[14/9] max-w-screen-sm'>
-      動態
+    <span className={`inline-block rounded-full px-2 mr-1 text-white font-normal ${cls}`}>
+      {children}
+    </span>
+  );
+}
+
+function SpotInfo() {
+  const spot = useAtomValue(viewItemAtom);
+
+  if (R.isEmpty(spot) || R.isNil(spot)) {
+    return (
+      <div className='bg-lime-300 aspect-[14/9]'>
+      </div>
+    );
+  }
+
+  return (
+    <div className='bg-lime-300 aspect-[14/9]'>
+      <strong>
+        <StateLabel spotState={spot.spotState}>{spot.spotState} </StateLabel>
+        {spot.spotTitle}
+      </strong>
+      <div>
+        <span data-user-id={spot.userId}>
+          USER
+        </span>
+
+        <time className='' dateTime={`${spot.createdAt}`}>
+          {formatDistanceToNow(spot.createdAt)}
+        </time>
+
+        {
+          spot.district ? <span>在 {spot.district}</span> : ''
+        }
+        {spot.action}
+      </div>
+
+      <div>
+        {spot.desc}
+      </div>
+
+      <div className='aspect-[4/3] ring-1 max-w-screen-sm'>
+        (IMG)
+      </div>
     </div>
   );
 }
@@ -49,6 +118,8 @@ function Followups({ items, today, oldestDate }: {
   today: Date,
   oldestDate: Date
 }) {
+  const setViewItem = useSetAtom(viewItemAtom);
+
   if (!items?.length) {
     return <p>沒有新發現</p>;
   }
@@ -75,7 +146,7 @@ function Followups({ items, today, oldestDate }: {
             <ul className='flex flex-row flex-wrap'>
               {subItems.map((i: RecentFollowupsItemProps) => (
                 <li key={i.spotId}>
-                  <MapPinIcon className={mapPinCls(i.spotState)} data-lat={i.lat} data-lon={i.lon} height={24} />
+                  <MapPinIcon className={`cursor-pointer ${mapPinCls(i.spotState)}`} onClick={() => setViewItem(i)} data-lat={i.lat} data-lon={i.lon} height={24} />
                 </li>
               ))}
             </ul> : null
@@ -97,9 +168,17 @@ export default function RecentFollowups({ items, today, oldestDate }: {
   today: Date,
   oldestDate: Date
 }) {
+  const setDefaultViewItem = useSetAtom(setDefaultViewItemAtom);
+
+  useEffect(() => {
+    if (items.length) {
+      setDefaultViewItem(items[0]);
+    }
+  }, [items, setDefaultViewItem])
+
   return (
     <>
-      <SpotInfo spot={items[0]} />
+      <SpotInfo />
       <Followups items={items} today={today} oldestDate={oldestDate} />
     </>
   );
--- src/lib/date-fp.ts
+++ src/lib/date-fp.ts
@@ -1,13 +1,31 @@
+import * as R from 'ramda';
+
+import { zhTW } from 'date-fns/locale/zh-TW';
+
+import {
+  formatDistanceToNow as formatDistanceToNowOriginal,
+} from 'date-fns';
+
 import {
   subDays,
   addHours,
   formatWithOptions as format,
-  formatISOWithOptions as formatISO
+  formatISOWithOptions as formatISO,
 } from 'date-fns/fp';
+
+// this is NOT functional style
+const formatDistanceToNow = (date: Date) => {
+  const options = {
+    locale: zhTW,
+    addSuffix: true,
+  };
+  return formatDistanceToNowOriginal(date, options);
+};
 
 export {
   subDays,
   addHours,
   format,
-  formatISO
+  formatISO,
+  formatDistanceToNow,
 };
