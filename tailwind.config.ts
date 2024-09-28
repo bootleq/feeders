36c26549d8368d64c6a9fc306b1b66ffa1d8e4a3
bootleq <bootleq@gmail.com>
2024-09-28 17:32:22 +0800

    Minor change tailwind font-family code formatting
    Use single quote.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,7 @@ const config: Config = {
           'ui-sans-serif', 'system-ui', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"
         ],
         mono: [
-          'Noto Mono TC',
-          'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', "Liberation Mono", "Courier New", 'monospace'
+          'Noto Mono TC', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', "Courier New", 'monospace'
         ]
       },
       backgroundImage: {
