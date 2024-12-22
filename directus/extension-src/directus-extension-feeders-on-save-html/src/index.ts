f23447a9e642f389e56bfe141202aa8cd27d0873
bootleq <bootleq@gmail.com>
2024-12-22 11:30:55 +0800

    Fill the implementation and add README

--- directus/extension-src/directus-extension-feeders-on-save-html/src/index.ts
+++ directus/extension-src/directus-extension-feeders-on-save-html/src/index.ts
@@ -1,11 +1,136 @@
 import { defineHook } from '@directus/extensions-sdk';
 
-export default defineHook(({ filter, action }) => {
-	filter('items.create', () => {
-		console.log('Creating Item!');
-	});
+import sanitizeHtml from 'sanitize-html';
+import * as htmlparser2 from 'htmlparser2';
+import { selectAll } from 'css-select';
+import type { Element } from 'domhandler';
 
-	action('items.create', () => {
-		console.log('Item created!');
-	});
+const targetFields: Record<string, string[]> = {
+  facts: ['desc', 'summary', 'origin'],
+  insights: ['content'],
+  laws: ['summary', 'penalty', 'judgements'],
+  blocks: ['content'],
+};
+
+const allowedAttributes = {
+  ...sanitizeHtml.defaults.allowedAttributes,
+  'iframe': [
+    'src',
+    'loading',
+    'allow',
+    'width',
+    'height',
+    'name',
+    {
+      name: 'allow',
+      multiple: true,
+      values: ['fullscreen', 'web-share'],
+    }
+  ],
+  '*': ['class'],
+};
+
+const allowedClasses = {
+  'table': ['feeders-mce-digit'],
+  'figure': ['feeders-mce-figure'],
+  'ol': ['feeders-mce-lengthy'],
+  'ul': ['feeders-mce-lengthy'],
+  'a': ['feeders-mce-lengthy'],
+  'p': ['feeders-mce-iframe'],
+  '*': ['font-mono', 'italic', 'underline', 'line-through', 'text-left'],
+};
+
+const allowedIframeHostnames = [
+  'www.youtube.com',
+  'bootleq.github.io',
+];
+
+const disallowedTagsMode = 'discard';
+
+const tagsFigure = 'figure,figcaption'.split(',');
+const tagsTable = 'table,colgroup,col,thead,th,tbody,tr,td'.split(',');
+const tagsSimple = 'p,a,b,i,em,strong,blockquote,div,span,hr,ol,ul,li,br,code,kbd,details,summary'.split(',');
+const tagsAdvanced = tagsSimple.concat(tagsFigure).concat(tagsTable).concat('h2,h3,h4,img,iframe'.split(','));
+
+const modelTagsMapping: Record<string, string[]> = {
+  facts: tagsSimple,
+  insights: tagsAdvanced,
+  laws: tagsSimple,
+};
+
+const FILE_ID_FIELD_NAME = 'cms_file_ids';
+const fileSelectors: Record<string, string> = {
+  insights: '.feeders-mce-figure img[src]',
+};
+
+
+const eventScopes = Object.keys(modelTagsMapping).reduce((acc: string[], model: string) => {
+  acc = acc.concat([`${model}.items.create`, `${model}.items.update`]);
+  return acc;
+}, []);
+
+// Extract file (directus_files) id from Element,
+// For example <img> with src="http://localhost:8055/assets/b64f2e08-ff55-4f98-9b1a-33900ad0d809.png?"
+// has file id equals to the basename (without suffix) of image.
+const extractFileId = (node: Element, publicURL: string) => {
+  if (node.name === 'img') {
+    const { src } = node.attribs;
+    const prefix = `${publicURL}/assets/`;
+    if (!src || !src.startsWith(prefix)) {
+      return null;
+    }
+
+    const leaf = src.slice(prefix.length);
+    const id = leaf.match(/^[\w-]+/)?.[0];
+    return id;
+  }
+
+  return null;
+};
+
+export default defineHook(({ filter }, { env }) => {
+  const { PUBLIC_URL } = env;
+  if (!PUBLIC_URL) {
+    throw new Error("No PUBLIC_URL defined, need it to determine file id.");
+  }
+
+	for (const eventScope of eventScopes) {
+		filter(eventScope, run);
+	}
+
+	function run(input: any, { collection }: any) {
+    const fileSelector = fileSelectors[collection];
+    const fileIds: string[] = [];
+
+    for (const field of targetFields[collection] || []) {
+      const html = sanitize(input[field], collection);
+      input[field] = html;
+
+      if (fileSelector) {
+        const dom = htmlparser2.parseDOM(html);
+        const files = selectAll(fileSelector, dom).filter(node => node.type === 'tag');
+        files.forEach(file => {
+          const id = extractFileId(file as Element, PUBLIC_URL);
+          if (id) {
+            fileIds.push(id);
+          }
+        });
+      }
+    }
+    if (fileIds.length) {
+      input[FILE_ID_FIELD_NAME] = fileIds;
+    }
+
+		return input;
+	}
+
+	function sanitize(val: any, collection: string) {
+		switch (typeof val) {
+			case 'string':
+        const allowedTags = modelTagsMapping[collection] || tagsSimple;
+				return sanitizeHtml(val, { allowedTags, disallowedTagsMode, allowedAttributes, allowedClasses, allowedIframeHostnames });
+			default:
+        throw new Error(`Unexpect value type (${typeof val}) during sanitize.`);
+		}
+	}
 });
