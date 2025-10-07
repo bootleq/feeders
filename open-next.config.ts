2ef1fae0e6de03541269f316ae3109815ef367b3
bootleq <bootleq@gmail.com>
2025-10-07 11:00:24 +0800

    Use regional-cache on R2 incremental cache
    Problem: the cache store increases R2 class B operations usage a lot.
    
    Here try using regional-cache to help it, which uses CF's Cache API,
    should provide an additional layer before hitting R2.
    
    Ref
    https://opennext.js.org/cloudflare/caching#incremental-static-regeneration-isr

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,10 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
 
 export default defineCloudflareConfig({
-	incrementalCache: r2IncrementalCache,
+  incrementalCache: withRegionalCache(r2IncrementalCache, {
+    mode: "long-lived",
+    shouldLazilyUpdateOnCacheHit: false,
+  }),
 });
