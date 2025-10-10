4bf436850257eb4334e19441f320938261b15cbe
bootleq <bootleq@gmail.com>
2025-10-10 22:22:56 +0800

    Extend Textarea to allow change label alignment
    Default <label> alignment `items-center` sometimes looks odd when the
    <textarea> has many rows (i.e., is tall).
    
    Now allow specify `labelOpts: { align: 'start' }` to change it to
    `items-start` to make the <label> appears on top.

--- src/components/form/Inputs.tsx
+++ src/components/form/Inputs.tsx
@@ -10,7 +10,7 @@ import { atom, useAtomValue } from 'jotai';
 import { errorsAtom, metaAtom } from '@/components/form/store';
 
 export const labelCls = [
-  'inline-flex items-center justify-end my-1 whitespace-nowrap active:font-bold',
+  'inline-flex justify-end my-1 whitespace-nowrap active:font-bold',
   'aria-[invalid]:text-red-700 aria-[invalid]:font-bold aria-[invalid]:ring-red-200 aria-[invalid]:ring-opacity-80',
 ].join(' ');
 export const inputCls = [
@@ -31,6 +31,10 @@ export const tooltipTrigger = (
 
 export interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
   invalid?: boolean;
+}
+
+export interface LabelOptions {
+  align?: 'start' | 'center';
 }
 
 export function useFieldError(field: string) {
@@ -93,29 +97,31 @@ export function TextInput({ label, name, tooltip, type = 'text', inputProps = {}
   );
 }
 
-export function Textarea({ label, name, children, tooltip, inputProps = {} }: {
+export function Textarea({ label, name, children, tooltip, labelOpts = {}, inputProps = {} }: {
   label?: string,
   name: string,
   children?: React.ReactNode,
   tooltip?: React.ReactNode,
-  inputProps?: React.InputHTMLAttributes<HTMLTextAreaElement>,
+  labelOpts?: LabelOptions,
+  inputProps?: React.TextareaHTMLAttributes<HTMLTextAreaElement>,
 }) {
   const id = useId();
   const { className = '', name: _name, ...restProps } = inputProps;
+  const { align: labelAlign = 'center' } = labelOpts;
   const errors = useFieldError(name);
   const fieldName = useFieldNameTranslate(name);
 
   const invalid = errors?.length > 0;
-  const labelProps: LabelProps = {
+  const mergedLabelProps: LabelProps = {
     htmlFor: id,
-    className: labelCls,
+    className: `${labelCls} ${labelAlign === 'center' ? 'items-center' : 'items-start'}`,
     children: label || fieldName(name),
     ...(invalid ? { 'aria-invalid': true } : {})
   };
 
   return (
     <>
-      <label {...labelProps} />
+      <label {...mergedLabelProps} />
       <textarea id={id} name={name} className={`${inputCls} text-base ${className}`} {...restProps}>
         {children}
       </textarea>
