26113212ef9c5a8a12e87e354c095ab1621b5437
bootleq <bootleq@gmail.com>
2025-10-18 20:21:10 +0800

    Generate new migration from schema

---
+++ drizzle/20251018121717_add-pubtime-and-adjust-ids-to-fact-picks.sql
@@ -0,0 +1,17 @@
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_factPicks` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`state` text DEFAULT 'draft' NOT NULL,
+	`factIds` text DEFAULT '[]' NOT NULL,
+	`title` text,
+	`desc` text,
+	`createdAt` integer DEFAULT (unixepoch()) NOT NULL,
+	`publishedAt` integer,
+	`userId` text,
+	FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+INSERT INTO `__new_factPicks`("id", "state", "factIds", "title", "desc", "createdAt", "publishedAt", "userId") SELECT "id", "state", "factIds", "title", "desc", "createdAt", "publishedAt", "userId" FROM `factPicks`;--> statement-breakpoint
+DROP TABLE `factPicks`;--> statement-breakpoint
+ALTER TABLE `__new_factPicks` RENAME TO `factPicks`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;
