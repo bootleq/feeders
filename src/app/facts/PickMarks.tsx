0b736dc3f4d8c295edcf1f522ee57a401f7bdee2
bootleq <bootleq@gmail.com>
2025-10-21 01:33:58 +0800

    Extract Mark from PickMarks
    It is a single mark item, extract it out first, plan to make the old
    marks (not for Picks) also use this <Mark> component.

--- src/app/facts/PickMarks.tsx
+++ src/app/facts/PickMarks.tsx
@@ -3,24 +3,17 @@
 import * as R from 'ramda';
 import { useCallback, useEffect, useMemo, useRef } from 'react';
 import { useAtomValue, useSetAtom } from 'jotai';
+import Mark from './Mark';
 import type { Fact, FactMark } from './store';
 import type { RecentPicksItemProps } from '@/models/facts';
-import { findFactElement, clearMarkIndicators } from './utils';
 import { Tooltip, TooltipTrigger, TooltipContent } from '@/components/Tooltip';
 import {
   pickAtom,
   removePickMarkAtom,
   picksModeAtom,
-  timelineInterObserverAtom,
 } from './store';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 import tlStyles from './timeline.module.scss';
-
-const markDateCls = [
-  'font-mono text-sm whitespace-nowrap ml-px mr-1 px-1 rounded-md ring-1 cursor-pointer',
-  'text-red-950 bg-gradient-to-br from-rose-200 to-rose-100/80',
-  'hover:ring hover:text-black',
-].join(' ');
 
 type FactsDictionary = Record<string, FactMark>;
 
@@ -52,49 +45,6 @@ function sortByAnchor(ids: number[], dict: FactsDictionary) {
   return R.sortBy(
     (id: number) => R.toString(R.path([id, 'anchor'], dict))
   )(ids);
-}
-
-type PickMark = FactMark & {
-  onRemove: (e: React.MouseEvent<HTMLButtonElement>) => void,
-}
-
-function Mark({ id, anchor, title, onRemove }: PickMark) {
-  const interObserver = useAtomValue(timelineInterObserverAtom);
-
-  const onMouseEnter = useCallback((e: React.MouseEvent<HTMLLIElement>) => {
-    const el = e.currentTarget;
-    const { anchor } = el.dataset;
-    const fact = findFactElement(anchor);
-    const target = fact?.querySelector('[data-role="fact-date"]');
-    if (target) {
-      target.classList.add(tlStyles['peeking-target']);
-      interObserver?.observe(target);
-    }
-  }, [interObserver]);
-
-  const date = R.match(/fact-(.+)_\d+/, anchor)[1];
-  const datePadEnd = date?.length < 10 ? <span className=''>{'\u00A0'.repeat(10 - date.length)}</span> : '';
-
-  return (
-    <li className='flex items-center py-1' data-anchor={anchor} onMouseEnter={onMouseEnter} onMouseLeave={clearMarkIndicators}>
-      <a className={markDateCls} data-anchor={anchor} href={`#${anchor}`}>
-        {date}{datePadEnd}
-      </a>
-      <Tooltip placement='right'>
-        <TooltipTrigger className='mb-1 block truncate'>
-          <div className='text-xs truncate'>
-            {title}
-          </div>
-        </TooltipTrigger>
-        <TooltipContent className="p-1 text-xs rounded box-border w-max z-[1002] bg-slate-100 ring-1">
-          {title}
-        </TooltipContent>
-      </Tooltip>
-      <button className='btn p-px ml-auto hover:bg-white rounded-full hover:scale-125 hover:drop-shadow' aria-label='刪除' data-fact-id={id} onClick={onRemove}>
-        <XMarkIcon className='stroke-slate-700 stroke-2' height={16} />
-      </button>
-    </li>
-  );
 }
 
 export default function PickMarks({ pick, facts }: {
